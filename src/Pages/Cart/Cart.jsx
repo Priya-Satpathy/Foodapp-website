960cefc7801cafaa0ec31b20576875b787237c12
Priya-Satpathy <priyasatpathy429@gmail.com>
2025-11-14 21:32:49 +0530

    content added

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -23,9 +23,18 @@ const Cart = () => {
             if(cartItems[item._id]>0)
             {
               return(
-                <div className='cart-items-title cart-items-item'>
+                <div>
+                    <div className='cart-items-title cart-items-item'>
+                      <img src={item.image} alt="" />
                       <p>{item.name}</p>
+                      <p>${item.price}</p>
+                      <p>{cartItems[item._id]}</p>
+                      <p>${item.price*cartItems[item._id]}</p>
+                      <p onClick={()=>removeFromCart(item._id)} className='cross'>x</p>
                 </div>
+                <hr/>
+                </div>
+              
               )
             }
         })}
